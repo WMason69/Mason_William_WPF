b4814fe678c869d12490b4bc6fbc0b49c9c175ac
William Mason <wmason69@fullsail.edu>
2013-10-07 21:00:06 -0400

    Linking script.js and index.html files together

---
+++ AssociatingJS/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: WebDesignComputer
+ * Date: 10/6/13
+ * Time: 9:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+alert("Hello World!");
