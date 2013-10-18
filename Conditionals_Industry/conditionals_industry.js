fc0982e4ff11bb6ace4ce4c460f5911a29286165
William Mason <wmason69@fullsail.edu>
2013-10-17 20:49:10 -0400

    Preparing to establish Group A variables

--- Conditionals_Industry/conditionals_industry.js
+++ Conditionals_Industry/conditionals_industry.js
@@ -1,3 +1,5 @@
 /* Mason, William, Conditionals_Industry Assignment, 10/17/13, 5:07 AM */
 
-alert("Hello Industry!");
+/* Calculate which group of Web Programmers are most productive
+in a given week */
+
