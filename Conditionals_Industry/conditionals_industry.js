64fd1c38dea615554e1930824323d4ecf5804af6
William Mason <wmason69@fullsail.edu>
2013-10-17 21:21:05 -0400

    Preparing file structure for Conditionals Assignment

--- Conditionals_Industry/conditionals_industry.js
+++ Conditionals_Industry/conditionals_industry.js
@@ -3,3 +3,25 @@
 /* Calculate which group of Web Programmers are most productive
 in a given week */
 
+/* Establish variable prompts for number of jobs completed in a 1 week
+ period by three members of Group A */
+
+var  memberOneGroupA = prompt("How many jobs did member one of Group A " +
+				"complete this week?");
+console.log("Member one of Group A completed " + memberOneGroupA + " jobs" +
+				" this week.");
+
+var  memberTwoGroupA = prompt("How many jobs did member two of Group A " +
+				"complete this week?");
+console.log("Member two of Group A completed " + memberTwoGroupA + " jobs" +
+				" this week.");
+
+var  memberThreeGroupA = prompt("How many jobs did member three of Group A " +
+				"complete this week?");
+console.log("Member Three of Group A completed " + memberThreeGroupA + " jobs" +
+				" this week.");
+
+var totalJobsGroupA = Number(memberOneGroupA) + Number(memberTwoGroupA) +
+				Number(memberThreeGroupA);
+console.log("The total number of jobs completed this week by Group A " +
+				"members is " + totalJobsGroupA + ".");
