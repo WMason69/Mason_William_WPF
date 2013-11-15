d545881571499e876fd9a396fc9ec789ef355c7f
William Mason <wmason69@fullsail.edu>
2013-11-14 21:22:15 -0500

    define and declare amount of average  dollar cost per job for client B

--- Conditionals_industry/conditionals_industry.js
+++ Conditionals_industry/conditionals_industry.js
@@ -24,3 +24,6 @@ console.log("We completed " + clientC + " jobs for Client C over the past" +
 //define and declare amount of average  dollar cost per job for client A
 var avgJobCostClientA=prompt("Please enter the per job cost for \n " +
 				" Client A projects");
+//define and declare amount of average  dollar cost per job for client B
+var avgJobCostClientB=prompt("Please enter the per job cost for \n " +
+				" Client B projects");
