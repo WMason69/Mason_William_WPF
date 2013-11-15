f8d0ceea85d1b0908887824d560991b6e9ced1e2
William Mason <wmason69@fullsail.edu>
2013-11-14 20:52:43 -0500

    declared and defined variable for clientA

--- Conditionals_industry/conditionals_industry.js
+++ Conditionals_industry/conditionals_industry.js
@@ -1,3 +1,13 @@
 /*Conditionals Assignment_Industry_ William Mason_ Date: 11/13/13 */
 
 alert("Conditionals Industry!");
+
+/* Write code that determines which client produced the most money for
+				my web design firm in a one year period*/
+
+//declaring and defining variable for Client A
+var clientA=prompt("Please enter How many jobs we did for Client A in \n" +
+				"a one year period");
+// print out user input from prompt to console
+console.log("We completed " + clientA + " jobs for Client A over the past" +
+				" year.");
