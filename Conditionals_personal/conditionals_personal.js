a45e961bb5463066fe3cf2e59ce6ad90940772db
William Mason <wmason69@fullsail.edu>
2013-11-16 08:23:44 -0500

    declared and defined noodles and sauce variable and printed out result to console and alerted user.

--- Conditionals_personal/conditionals_personal.js
+++ Conditionals_personal/conditionals_personal.js
@@ -1,5 +1,13 @@
 /*Conditionals Assignment_Personal_William Mason_ Date: 11/13/13 */
 
 /* Write a calculation that determines how many meatballs to put in a
-				adjustable size spaghetti recipe with 5 meatballs being used for every
-				8 oz of spaghetti noodles and sauce */
+				adjustable size spaghetti recipe with user determined number of
+				meatballs being used for every user determined portion size oz of
+				spaghetti noodles and sauce */
+
+//declare and user defined noodles and sauce portion size variable/
+var nooSauSize = prompt("Please enter the noodles and sauce portion size \n" +
+				"in number of oz.");
+//print out to console user entered data
+console.log("The noodles and sauce portion size is " + nooSauSize + "oz.");
+alert("The noodles and sauce portion size is " + nooSauSize + "oz.");
