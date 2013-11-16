6a650ec50336c6cc62a791412c2ba6f24d4f0296
William Mason <wmason69@fullsail.edu>
2013-11-16 09:03:11 -0500

    declare variable for number of meatballs that is user defined through a prompt function

--- Conditionals_personal/conditionals_personal.js
+++ Conditionals_personal/conditionals_personal.js
@@ -11,3 +11,9 @@ var nooSauSize = prompt("Please enter the noodles and sauce portion size \n" +
 //print out to console user entered data
 console.log("The noodles and sauce portion size is " + nooSauSize + "oz.");
 alert("The noodles and sauce portion size is " + nooSauSize + "oz.");
+
+/*declare variable for number of meatballs that is user defined through
+a prompt function*/
+var numMeatballsPortion = prompt("Please enter the number of meatballs \n" +
+				" that you use for each portion size of noodles and sauce.");
+
