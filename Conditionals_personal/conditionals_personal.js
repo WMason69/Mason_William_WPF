ce39c3376ec525aab247b092da7fbd804b36db93
William Mason <wmason69@fullsail.edu>
2013-11-16 08:00:52 -0500

    Comment on what I am about to calculate

--- Conditionals_personal/conditionals_personal.js
+++ Conditionals_personal/conditionals_personal.js
@@ -1,3 +1,5 @@
 /*Conditionals Assignment_Personal_William Mason_ Date: 11/13/13 */
 
-alert("Conditionals Personal!");
+/* Write a calculation that determines how many meatballs to put in a
+				adjustable size spaghetti recipe with 5 meatballs being used for every
+				8 oz of spaghetti noodles and sauce */
