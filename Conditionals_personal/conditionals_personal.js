a580318efd7fd1a02026dbb307b3f5d803c5cacf
William Mason <wmason69@fullsail.edu>
2013-11-17 15:44:43 -0500

    declare variable that will define the average portion size

--- Conditionals_personal/conditionals_personal.js
+++ Conditionals_personal/conditionals_personal.js
@@ -2,23 +2,34 @@
 
 /* Write a calculation that determines how many meatballs to put in a
 				adjustable size spaghetti recipe with user determined number of
-				meatballs being used for every user determined portion size oz of
+				meatballs being used for every user determined meal size oz of
 				spaghetti noodles and sauce */
 
 //declare and user defined noodles and sauce portion size variable/
-var nooSauSize = prompt("Please enter the noodles and sauce portion size \n" +
+var nooSauSize = prompt("Please enter the noodles and sauce meal size \n" +
 				"in number of oz.");
+
 //print out to console user entered data
-console.log("The noodles and sauce portion size is " + nooSauSize + "oz.");
-alert("The noodles and sauce portion size is " + nooSauSize + "oz.");
+console.log("The noodles and sauce meal size is " + nooSauSize + " oz.");
+alert("The noodles and sauce meal size is " + nooSauSize + " oz.");
 
 /*declare variable for number of meatballs that is user defined through
 a prompt function*/
-var numMeatballsPortion = prompt("Please enter the number of meatballs \n" +
-				" that you use for each portion size of noodles and sauce.");
+var numMeatballsPortion = prompt("Please enter the number of meatballs that you will\n" +
+				" use for each " + nooSauSize + " oz." +  " of noodles and sauce.");
+
 /*print out user entered information to console and give feedback to user
 using the alert function*/
-console.log("The number of meatballs that you use for each portion \n" +
-				" size of noodles and sauce is " + numMeatballsPortion + ".");
-alert("The number of meatballs that you use for each portion \n" +
-				" size of noodles and sauce is " + numMeatballsPortion + ".");
+console.log("The number of meatballs that you will use for each " + nooSauSize +
+				" oz." + " of noodles and sauce is " + numMeatballsPortion + ".");
+alert("The number of meatballs that you will use for each " + nooSauSize + " oz."  +
+				" of noodles and sauce is " + numMeatballsPortion + ".");
+
+/*declare variable and define as an expression that calculates number of
+portions in a meal size*/
+
+var avgPortSize = Number("4");
+console.log("The average portion size is " + avgPortSize + " oz.");
+
+
+
