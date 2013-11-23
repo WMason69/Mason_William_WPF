f2b5b706971a1cbb11e211f52089a85a8421a92f
William Mason <wmason69@fullsail.edu>
2013-11-23 17:55:07 -0500

    Starting fresh to create code for functions industry assignment that calculate average miles per gallon between three vehicles

--- Functions_industry/industry.js
+++ Functions_industry/industry.js
@@ -1,17 +1,5 @@
 /* William Mason, Functions Assignment - Industry, November 20, 2013 */
 
 
-//Code that calculate average miles per gallon between three vehicles/
-
-//define named function with 3 parameters
-function vehicleMilage(vehicle1, vehicle2, vehicle3){
-
-//declare and define variable that holds number of vehicles
-var numVehicles = 3;
-
-/*set up variables that uses an expression to calculate formula for
-average miles per gallon*/
-
-var averageMpg = (vehicle1 + vehicle2 + vehicle3)/numVehicles;
-}
+/*Code that calculate average miles per gallon between three vehicles*/
 
