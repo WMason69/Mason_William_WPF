95053a3feaa80810e171decd6c558ca6922eb27a
William Mason <wmason69@fullsail.edu>
2013-11-23 18:05:06 -0500

    sep up variable to hold expression using user input arguments

--- Functions_industry/industry.js
+++ Functions_industry/industry.js
@@ -5,3 +5,7 @@
 
 //set up basic function structure and parameter
 function avgMpg(vehicleA, vehicleB, vehicleC){
+//sep up variable to hold expression using user input arguments
+
+var averageMilesGallon = (Number(vehicleA) + Number(vehicleB) + Number(vehicleC))/3
+}
