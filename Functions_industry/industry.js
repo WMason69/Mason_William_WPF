84529073b4fb5a4dc0ab68b9ca0f97653fe8a8c6
William Mason <wmason69@fullsail.edu>
2013-11-23 18:11:22 -0500

    console print out of each vehicles miles per gallon and console print out of all vehicles combined miles per gallon

--- Functions_industry/industry.js
+++ Functions_industry/industry.js
@@ -5,7 +5,19 @@
 
 //set up basic function structure and parameter
 function avgMpg(vehicleA, vehicleB, vehicleC){
+
 //sep up variable to hold expression using user input arguments
 
 var averageMilesGallon = (Number(vehicleA) + Number(vehicleB) + Number(vehicleC))/3
+
+// console print out of each vehicles miles per gallon
+				console.log("Vehicle A gets " + Number(vehicleA) + " miles per gallon.");
+				console.log("Vehicle B gets " + Number(vehicleB) + " miles per gallon.");
+				console.log("Vehicle C gets " + Number(vehicleC) + " miles per gallon.");
+
+// console print out of all vehicles combined miles per gallon
+				console.log("The average miles per gallon of Vehicles A, B and C is " +
+								averageMilesGallon +	" mpg.");
+
+
 }
