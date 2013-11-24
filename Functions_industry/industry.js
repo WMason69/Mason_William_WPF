4b359d80a79a7a69084f6dd900cbac11a5e5d641
William Mason <wmason69@fullsail.edu>
2013-11-24 17:02:33 -0500

    if statement using logical operator to return Boolean values to see if Vehicle A gas mileage is the best or less than I thought

--- Functions_industry/industry.js
+++ Functions_industry/industry.js
@@ -1,5 +1,7 @@
 /* William Mason, Functions Assignment - Industry, November 20, 2013 */
 
+var combinedMpg = avgMpg (prompt("Please enter miles per gallon for Vehicle A."), prompt("Please enter miles per gallon for Vehicle B."),
+				prompt("Please enter miles per gallon for Vehicle C.")); //storing the return value of function
 
 /*Code that calculate average miles per gallon between three vehicles*/
 
@@ -8,7 +10,7 @@ function avgMpg(vehicleA, vehicleB, vehicleC){
 
 //sep up variable to hold expression using user input arguments
 
-var averageMilesGallon = (Number(vehicleA) + Number(vehicleB) + Number(vehicleC))/3
+var averageMilesGallon = (Number(vehicleA) + Number(vehicleB) + Number(vehicleC))/3;
 
 // console print out of each vehicles miles per gallon
 				console.log("Vehicle A gets " + Number(vehicleA) + " miles per gallon.");
@@ -22,7 +24,7 @@ var averageMilesGallon = (Number(vehicleA) + Number(vehicleB) + Number(vehicleC)
 /*series of if else conditions and console print outs related to evaluated
 conditions*/
 				if(vehicleA >= averageMilesGallon){console.log("Vehicle A meets the " +
-								"minimum required miles per gallon.")
+								"minimum required miles per gallon.");
 				}else{console.log("Vehicle A does not meet the	minimum required miles per gallon.");
 
 				}if(vehicleB >= averageMilesGallon){console.log("Vehicle B meets the " +
@@ -30,8 +32,14 @@ conditions*/
 				}else{console.log("Vehicle B does not meet the	minimum required miles per gallon.");
 
 				}if(vehicleC >= averageMilesGallon){console.log("Vehicle C meets the " +
-								"minimum required miles per gallon.")
+								"minimum required miles per gallon.");
 				}else{console.log("Vehicle C does not meet the	minimum required miles per gallon.");
 
 				}
+}
+/*if statement using logical operator to return Boolean values to see if Vehicle A
+ gas mileage is the best or less than I thought */
+if(vehicleA > vehicleB && vehicleA > vehicleC){console.log("Vehicle A gets the best gas mileage.");
+
+}else{console.log("Vehicle A's gas mileage is not as good as I thought.")
 }
