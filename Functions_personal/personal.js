2e7e2381a8918dad416f6d09c4791372b8b613a2
William Mason <wmason69@fullsail.edu>
2013-11-24 18:27:46 -0500

    call function and establish arguments using prompts

--- Functions_personal/personal.js
+++ Functions_personal/personal.js
@@ -3,8 +3,23 @@
 /* Code that calculates what toppings a person might want on
 a pizza */
 
-//setup function for pizza ingredients
+//setup function for pizza ingredient parameters
 
 function pizzaIngredients(pepporoni,sausage,extraCheese,onions){
+				  var deluxePizza = ("I would like pepperoni, sausage, extra cheese and onions." +
+								  " Also known as The Deluxe.");
+				  var normalPizza = "I only want pepperoni and extra cheese. Also known" +
+								  " as The Norm.";
+						var specialPizza = "I'll have sausage, extra cheese and onions. Better" +
+										" known as The Special.";
+
 
 };
+
+//call function and establish arguments using prompts
+pizzaIngredients(
+				prompt("Please enter either Yes or No if you want pepperoni on your pizza."),
+				prompt("Please enter either Yes or No if you want sausage on your pizza."),
+				prompt("Please enter either Yes or No if you want extra cheese on your pizza."),
+				prompt("Please enter either Yes or No if you want onions on your pizza.")
+)
