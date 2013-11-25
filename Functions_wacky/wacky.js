51cd43fe4018b28439236ac4faf7cb64354931fc
William Mason <wmason69@fullsail.edu>
2013-11-24 23:58:25 -0500

    //expression for returning function

--- Functions_wacky/wacky.js
+++ Functions_wacky/wacky.js
@@ -1,3 +1,15 @@
 // William Mason, Functions Assignment - Wacky, Date: 11/20/13
 
-//Code to calculate a wacky solution using an anonymous prompt
+//Code to calculate a wacky solution using an anonymous function
+
+var pastaPieces = function(noodles,meatballs, breadSlices){
+				var totalMeal = (noodles * meatballs); //expression for returning function
+				console.log("The total number of meatballs multiplied by the number" +
+								" of noodles is " + totalMeal + "."+ "There are also " + breadSlices + " slices of bread.");
+
+				console.log(totalMeal);//print out to console
+				return totalMeal; //spitting out function
+};
+pastaPieces(7,7,7); //function argument
+
+
